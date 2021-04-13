d575d988ab077a032506def9407189c1683ec927
Gavin Broekhuis <gavin.broekhuis@witteveenbos.com>
2021-04-13 14:48:49 +0200

    :hammer: Add Episodes to MediaFixtures

--- src/DataFixtures/MediaFixtures.php
+++ src/DataFixtures/MediaFixtures.php
@@ -2,14 +2,17 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\TvShowEpisode;
+use App\Entity\TvShowSeason;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Media;
 use App\Entity\Movie;
-use App\Entity\TvShow;
+use App\DataFixtures\TvShowFixtures;
 use Faker\Factory;
 
-class MediaFixtures extends Fixture
+class MediaFixtures extends Fixture implements DependentFixtureInterface
 {
     /* @var \Faker\Factory */
     protected $faker;
@@ -25,6 +28,29 @@ class MediaFixtures extends Fixture
             $manager->persist($movie);
         }
 
+        $seasons = $manager->getRepository(TvShowSeason::class)->findAll();
+        foreach($seasons as $season) {
+            $this->faker = Factory::create();
+            $numberOfEpisodes = $this->faker->numberBetween(8,14);
+
+            for($e=0; $e<$numberOfEpisodes; $e++) {
+                $episode = new TvShowEpisode();
+                $episode->setTitle($this->faker->company);
+                $episode->setDuration($this->faker->numberBetween(15,40));
+                $episode->setSeason($season);
+                $episode->setPosterPath('posterPathEpisode');
+                $episode->setEpisodeNumber($e+1);
+                $manager->persist($episode);
+            }
+        }
+
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TvShowFixtures::class,
+        ];
     }
 }
